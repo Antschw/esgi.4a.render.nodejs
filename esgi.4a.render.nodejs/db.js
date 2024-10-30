a69f1d68721c9289b21a379b6e52f03ccd5b600a
AntSch <antoine97410@hotmail.fr>
2024-10-30 11:40:59 +0100

    Rework the link to the database

--- esgi.4a.render.nodejs/db.js
+++ esgi.4a.render.nodejs/db.js
@@ -3,7 +3,8 @@ const { Sequelize } = require('sequelize');
 
 // Database
 const sequelize = new Sequelize(
-  `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}/${process.env.DB_NAME}`,
+  // `postgresql://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOSTNAME}/${process.env.DB_NAME}`,
+  process.env.DATABASE_URL,
   {
     dialect: 'postgres',
     dialectOptions: {
